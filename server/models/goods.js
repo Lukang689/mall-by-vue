c14b39cfe0ecd80f18926fdf1c9607932d6cecce
Lukang689 <lukang8596@outlook.com>
2017-10-30 19:19:21 +0800

    /goos is ok

--- server/models/goods.js
+++ server/models/goods.js
@@ -1,1 +1,11 @@
-module.exports
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var productSchema = new Schema({
+    "productId" :String,
+    "productName" : String,
+    "salePrice" : Number,
+    "productImage" : String
+});
+
+module.exports = mongoose.model('Good',productSchema);
---
+++ server/routes/goods.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Goods = require('../models/goods');
+
+mongoose.connect('mongodb://127.0.0.1:27017/dumall');
+
+mongoose.connection.on("connected",()=>{
+    console.log("数据库连接成功")
+});
+
+mongoose.connection.on("err",()=>{
+    console.log("数据库连接出错")
+});
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("数据库连接已断开")
+});
+/* GET home page. */
+router.get('/', (req, res, next)=>{
+//   res.render('index', { title: 'Express' });
+    //  res.send("数据库response");
+    Goods.find({},(err,doc)=>{
+        if(err){
+            res.json({
+                status:'1',
+                msg:err.message
+            });
+        }else{
+            res.json({
+                status:'0',
+                msg:'',
+                result:{
+                    count:doc.length,
+                    list:doc
+                }
+            })
+        }
+    })
+});
+
+module.exports = router;
